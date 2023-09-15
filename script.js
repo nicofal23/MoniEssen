8bf029ddd0a02f99e9f62e0e7b539157dd72de62
Nicolas Falciglio <136000929+nicofal23@users.noreply.github.com>
2023-09-14 21:19:13 -0300

    javascript

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+const elementoAnimado = document.querySelector('.texto');
+let lastScrollPosition = 0;
+let animationActive = false;
+
+function iniciarAnimacion() {
+    if (!animationActive) {
+        elementoAnimado.classList.add('scroll-animation');
+        animationActive = true;
+    }
+}
+
+function detenerAnimacion() {
+    if (animationActive) {
+        elementoAnimado.classList.remove('scroll-animation');
+        animationActive = false;
+    }
+}
+
+window.addEventListener('scroll', () => {
+    const currentScrollPosition = window.scrollY;
+
+    if (currentScrollPosition > lastScrollPosition) {
+        // El usuario está haciendo scroll hacia abajo
+        iniciarAnimacion();
+    } else {
+        // El usuario está haciendo scroll hacia arriba
+        detenerAnimacion();
+    }
+
+    lastScrollPosition = currentScrollPosition;
+});
 
