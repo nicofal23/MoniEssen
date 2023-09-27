3f269774fef09b9faf14f464e7f7b08d09a0d418
Nicolas Falciglio <falciglionicolas@gmail.com>
2023-09-27 16:00:52 -0300

    correcciones

--- script.js
+++ script.js
@@ -23,6 +23,11 @@ document.addEventListener("DOMContentLoaded", function () {
         cantidadCarrito.textContent = cantidadProductos;
     }
 
+    // Función para actualizar el almacenamiento local con el contenido del carrito
+    function actualizarLocalStorage() {
+        localStorage.setItem("carrito", JSON.stringify(carritoCompras));
+    }
+
     // Agregar event listeners a los botones "Agregar al Carrito"
     botonesAgregarCarrito.forEach((boton) => {
         boton.addEventListener("click", function (event) {
@@ -33,25 +38,75 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     // Event listener para redirigir al usuario a la página de detalles del carrito
-  // Código para redirigir a la página de carrito cuando se hace clic en un botón u elemento con el id "carrito"
-document.getElementById("carrito").addEventListener("click", function () {
-    window.location.href = "carrito.html";
+    // Código para redirigir a la página de carrito cuando se hace clic en un botón u elemento con el id "carrito"
+    document.getElementById("carrito").addEventListener("click", function () {
+        window.location.href = "carrito.html";
+    });
 });
 
-// Código para mostrar los productos en la página de carrito.html
-if (window.location.href.endsWith("carrito.html")) {
+document.addEventListener("DOMContentLoaded", function () {
     const detallesCarrito = document.getElementById("detallesCarrito");
+    
+    // Obtener el carrito de compras almacenado en el almacenamiento local
     const carritoCompras = JSON.parse(localStorage.getItem("carrito")) || {};
 
+    // Recorre los productos en el carrito y crea elementos HTML para mostrarlos
     for (const idProducto in carritoCompras) {
-        const producto = carritoCompras[idProducto];
-        const productoHTML = getElementById("div");
+        const cantidad = carritoCompras[idProducto];
+        const productoHTML = document.createElement("div");
         const parrafo = document.createElement("p");
-        parrafo.textContent = `${producto.nombre}: ${producto.cantidad}`;
-        parrafo.classList.add("detalle-producto"); // Agregar una clase CSS
+        parrafo.textContent = `Nombre del producto: ${idProducto}, Cantidad: ${cantidad}`;
+        parrafo.classList.add("detalle-producto");
         productoHTML.appendChild(parrafo);
         detallesCarrito.appendChild(productoHTML);
     }
-}
 
+    //evento para enviar el msj por whatsapp
+    document.addEventListener("DOMContentLoaded", function () {
+        const boton = document.getElementById("boton");
+        boton.addEventListener("click", function (event) {
+            event.preventDefault(); // Evita el comportamiento por defecto del enlace
+            // Obtener el carrito de compras almacenado en el almacenamiento local
+            const carritoCompras = JSON.parse(localStorage.getItem("carrito")) || {};
+            // Crear una lista de productos para el mensaje
+            const listaProductos = [];
+            for (const idProducto in carritoCompras) {
+                const cantidad = carritoCompras[idProducto].cantidad;
+                const nombreProducto = carritoCompras[idProducto].nombre;
+                listaProductos.push(`${nombreProducto} - Cantidad: ${cantidad}`);
+            }
+            // Crear el mensaje con la lista de productos
+            const mensajeProductos = listaProductos.join("\n");
+            // URL base de WhatsApp
+            const urlWhatsAppBase = "https://api.whatsapp.com/send?phone=+5493416488215&text=";
+            // Combinar la URL base y el mensaje predefinido
+            const urlWhatsApp = urlWhatsAppBase + encodeURIComponent("Me interesan los siguientes productos") + " " + encodeURIComponent(mensajeProductos);
+            // Redirigir al usuario a WhatsApp con el mensaje
+            window.location.href = urlWhatsApp;
+        });
+    });
 });
+
+
+    // Event listener para redirigir al usuario a la página de detalles del carrito
+    // Código para redirigir a la página de carrito cuando se hace clic en un botón u elemento con el id "carrito"
+    // document.getElementById("carrito").addEventListener("click", function () {
+    //     window.location.href = "carrito.html";
+    // });
+
+    // // Código para mostrar los productos en la página de carrito.html
+    // if (window.location.href.endsWith("carrito.html")) {
+    //     const detallesCarrito = document.getElementById("detallesCarrito");
+    //     const carritoCompras = JSON.parse(localStorage.getItem("carrito")) || {};
+
+    //     for (const idProducto in carritoCompras) {
+    //         const cantidad = carritoCompras[idProducto];
+    //         const productoHTML = document.createElement("div");
+    //         const parrafo = document.createElement("p");
+    //         parrafo.textContent = `ID del producto: ${idProducto}, Cantidad: ${cantidad}`;
+    //         parrafo.classList.add("detalle-producto");
+    //         productoHTML.appendChild(parrafo);
+    //         detallesCarrito.appendChild(productoHTML);
+    //     }
+    // }
+
